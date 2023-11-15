358b7a0773467d26c04f3d060f2580c844460ad5
Татьяна <tanya@MacBook-Air-Vladimir.local>
2023-11-15 13:18:31 +0500

    init commit

---
+++ Arifm_if_else/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+/**
+Console.WriteLine();//вывод на экран с переходом на след строку
+Console.Write("");//вывод на экран без перехода на след строку
+Console.ReadLine();//ввод на клавиатуры
+/**/
+//вывод на экран текста
+Console.WriteLine("Привет, мир");
+//вывод на экран числа
+int a = 5;
+Console.WriteLine(a);
+//вывод на экран значения выражения
+
+int b = 5, x = 4;
+Console.WriteLine(x + 2 * b - 4 * a);
+Console.WriteLine(Math.Round(Math.E,1));
+Console.ReadKey();//задержка экрана
